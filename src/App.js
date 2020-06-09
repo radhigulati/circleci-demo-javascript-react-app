e2ab782e471bad567e3acc3774730931bcca3756
Radhika Gulati <radhigulati@gmail.com>
2020-06-09 12:26:17 -0700

    changed yaml for approval jobs

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Header title="Aww Gram"/>
+        <Header title="Aww Did This Work Gram"/>
         <Container>
           <Card cards={ this.state.cards } />
         </Container>
