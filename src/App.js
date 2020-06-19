7194f3641fc82ec5a0c8e7de48a394dca68eb40a
Radhika Gulati <radhigulati@gmail.com>
2020-06-19 16:51:32 -0700

    Changed the title of app to orbs are cool

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Header title="Aww Gram"/>
+        <Header title="Orbs are cool!!!"/>
         <Container>
           <Card cards={ this.state.cards } />
         </Container>
