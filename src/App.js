93ad517ed9cdee89c018060af2c30f224413ba13
Radhika Gulati <radhigulati@gmail.com>
2020-06-19 15:27:35 -0700

    text change again

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Header title="Aww Gram (This change is on 7/17)"/>
+        <Header title="Aww Gram"/>
         <Container>
           <Card cards={ this.state.cards } />
         </Container>
