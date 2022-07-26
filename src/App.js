938c52fc3bb0c96a0a19ce3d901cf7bccb05b965
Radhika Gulati <radhigulati@gmail.com>
2022-07-26 16:43:08 -0700

    made title change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Header title="Orbs are awesome!"/>
+        <Header title="Orbs are cool!"/>
         <Container>
           <Card cards={ this.state.cards } />
         </Container>
